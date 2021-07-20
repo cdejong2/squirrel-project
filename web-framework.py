01b5463046c040f28d92bae78a41e6505edce44d
Onyii Nnebe <onyii.nnebe@gmail.com>
2021-07-20 11:30:18 -0400

    Update web-framework.py

--- web-framework.py
+++ web-framework.py
@@ -6,10 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', subtitle='Home Page', text='Welcome to the Squirrel Finder!')
 
-@app.route("/about")
-def second_page():
-    return render_template('about-page.html', subtitle='About', text='This is the about page')
+@app.route("/learn")
+def learn():
+    return render_template('learn.html', subtitle='Learn', text='This is where you can learn about the squirrels around you')
 
+@app.route("/listen")
+def learn():
+    return render_template('listen.html', subtitle='Listen', text='This is where you can listen to different squirrels')
+
+@app.route("/login/register")
+def login():
+    return render_template('login-register.html', subtitle='Login/Register')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
