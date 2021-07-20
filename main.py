4d17dfc83cc4aaef22486d86eea1cc80cecced36
Tina de Jong <cdejong2@jhu.edu>
2021-07-20 15:40:18 +0000

    got rid of username in main.py - not in form

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ class User(db.Model):
         return False
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.email}')"
 
 
 @app.route("/")
